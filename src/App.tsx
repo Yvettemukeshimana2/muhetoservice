1aa5e0d95a41cdc2fc5cf8ee9f5f6acbc5e60b9f
Yvettemukeshimana2 <yvettemukeshimana79@gmail.com>
2024-05-10 09:20:55 +0200

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,29 @@
 
  const App: React.FC = () => {
    return (
-     <div className="min-h-screen w-full bg-gray-100 flex flex-col h-screen">
-       <div className="text-1xl bg-green-900 h-auto flex items-center justify-between p-2">
+     <div className=" bg-gray-100 over flex flex-col ">
+       <div className="fixed top-0 left-0 w-full bg-green-900 p-4 flex justify-between items-center">
          <p className="text-white text-4xl">paradox</p>
-         <div className="flex space-x-5 mr-16 justify-between">
-           <button className="text-white hover:text-black w-24 hover:bg-green-900">
+         <div className="flex space-x-5">
+           <button className="text-white hover:text-black hover:bg-green-900 px-4 py-2">
              Home
            </button>
-           <button className="text-white w-24 hover:text-black hover:bg-green-900">
+           <button className="text-white hover:text-black hover:bg-green-900 px-4 py-2">
              About me
            </button>
-           <button className="text-white w-28 hover:text-black hover:bg-green-900">
+           <button className="text-white hover:text-black hover:bg-green-900 px-4 py-2">
              Project
            </button>
-           <button className="text-white w-28 hover:text-black hover:bg-green-900">
+           <button className="text-white hover:text-black hover:bg-green-900 px-4 py-2">
              Contact
            </button>
          </div>
        </div>
+
        {/* Content goes here */}
-       <div className="flex-1 bg-customGreen-950 h-auto justify-start w-auto text-left">
+       <div className=" overflow-x-hidden w-screen h-screen bg-customGreen-950 text-left p-4 justify-between">
          <div className="flex flex-row">
-           <div className="mt-8 ">
+           <div className="mt-8">
              <div className="text-white font-semibold text-2xl ml-12 mt-4">
                Hello It’s me
              </div>
@@ -49,15 +50,16 @@
            </div>
 
            <div>
-             <div className="flex justify-center items-center w-60 h-60  mt-10  rounded-full bg-customCyan-200">
+             <div className="flex justify-center items-center w-60 h-60 mt-10  rounded-full bg-customCyan-200">
                <img className="h-30 w-40" src={im} alt="Paradox" />
              </div>
            </div>
          </div>
-         < button className="rounded-full mb-10 mt-20 bg-indigo-500 w-40 ml-12">
+         <button className="rounded-full mb-10 mt-20 bg-indigo-500 w-40 ml-12">
            <h6 className="text-center">Download a CV</h6>
          </button>
        </div>
+
        <Aboutme />
      </div>
    );
