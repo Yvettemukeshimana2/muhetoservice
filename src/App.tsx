10b3ac8899f7210d86fc1a237a7b0933801c550e
Yvettemukeshimana2 <yvettemukeshimana79@gmail.com>
2024-05-17 10:25:50 +0200

    cot

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
- import React, { useState, useEffect } from "react";
+ import React, { useState } from "react";
  import "./App.css";
  import im from "./images/ic - Copy.png";
  import Aboutme from "./assets/Component/Aboutme";
